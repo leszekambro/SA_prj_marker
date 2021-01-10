e25a64ba4f3bd883bc7d95cff845d78a813c2cce
leszekambro <leszek.ambroziak@gamil.com>
2021-01-10 07:03:04 -0800

    marker presence checking added

--- src/aaa.cpp
+++ src/aaa.cpp
@@ -11,11 +11,15 @@ int main(int argc, char** argv){
   tf::TransformListener listener;
 
   ros::Rate rate(10.0);
+  
+  bool nie_ma = true; 
   while (node.ok()){
     tf::StampedTransform transform;
+    std::cout << std::boolalpha << nie_ma << std::endl;
     try{
       listener.lookupTransform("/ardrone_base_bottomcam", "/ar_marker_0",  
                                ros::Time(0), transform);
+      nie_ma = false;
       std::cout << transform.getOrigin().getX()<< transform.getOrigin().getY()<< transform.getOrigin().getZ()
       << std::endl;
       double roll, pitch, yaw;
@@ -28,7 +32,9 @@ int main(int argc, char** argv){
       ROS_ERROR("%s",ex.what());
       ros::Duration(1.0).sleep();
     }
-
+    catch (tf::LookupException& ex) { 
+		nie_ma = true;
+		}
 
     rate.sleep();
   }
